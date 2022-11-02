7956a4ccb5d0b6b8ea95495afa608e98d34ba2e7
jingsheng yao <416348003@qq.com>
2022-11-02 23:54:36 +0800

    添加纪念日的添加、删除功能
    在首页添加了添加纪念日的添加、删除功能，但首页的布局尚未完善，同时任务监督完成功能未完善

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,12 +4,12 @@ App({
 
     this.globalData = {
       //记录使用者的openid
-      _openidA: 'onsgK5XqCMLbYXudVgh3xjV4kgWw',
-      _openidB: 'onsgK5Zc7t-bFdnFk3_krqdIRZYY',
+      _openidA: 'oJhoW46SZTyRLpcsrWdzcKDWEsa0',
+      _openidB: 'oJhoW45Y2FNgeTMAGEqw5ZqezeZQ',
 
       //记录使用者的名字
-      userA: '卡比',
-      userB: '瓦豆',
+      userA: '胸胸',
+      userB: 'mimi',
 
       //用于存储待办记录的集合名称
       collectionMissionList: 'MissionList',
